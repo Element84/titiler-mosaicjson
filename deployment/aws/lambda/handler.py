a7f5cc8ba27c6ea6522997b7f6dfbddf6a76edc1
bick4ord <bick4ord@users.noreply.github.com>
2023-09-12 12:54:59 -0400

    Update deployment/aws/lambda/handler.py
    Co-authored-by: Phil Varner <pvarner@element84.com>

--- deployment/aws/lambda/handler.py
+++ deployment/aws/lambda/handler.py
@@ -13,8 +13,8 @@ logging.getLogger("mangum.http").setLevel(logging.ERROR)
 REQUEST_HOST_HEADER_OVERRIDE_ENV_VAR = "REQUEST_HOST_HEADER_OVERRIDE"
 
 def handler(event, context):
-    if REQUEST_HOST_HEADER_OVERRIDE_ENV_VAR in os.environ and os.environ[REQUEST_HOST_HEADER_OVERRIDE_ENV_VAR] != "":
-        event["headers"]["host"] = os.environ[REQUEST_HOST_HEADER_OVERRIDE_ENV_VAR]
+    if rhh := os.getenv(REQUEST_HOST_HEADER_OVERRIDE_ENV_VAR):
+        event["headers"]["host"] = rhh
 
     asgi_handler = Mangum(app, lifespan="auto")
     return asgi_handler(event, context)
